deb14b5b71306e92dea86cee8ca1df840b0449f5
KyuTQT <145522415+KyuTQT@users.noreply.github.com>
2023-10-06 10:36:39 +0800

    Implement math.random ranging from 1-3
    This will determine the choice of the computer for rps

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
 console.log("Hello World!");
+
+function getComputerChoice () {
+    let rpsRNG = Math.floor(Math.random() * 3) + 1; 
+    console.log(rpsRNG);
+    let rpsStringChoice;
+}
+
+getComputerChoice();
