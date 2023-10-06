188c2c67c846a6f93671cf37e4cce2ceb157450e
KyuTQT <145522415+KyuTQT@users.noreply.github.com>
2023-10-06 10:38:45 +0800

    Rename the variable for readability

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 console.log("Hello World!");
 
 function getComputerChoice () {
-    let rpsRNG = Math.floor(Math.random() * 3) + 1; 
+    let rpsChoice = Math.floor(Math.random() * 3) + 1; 
     console.log(rpsRNG);
-    let rpsStringChoice;
+
 }
 
 getComputerChoice();
